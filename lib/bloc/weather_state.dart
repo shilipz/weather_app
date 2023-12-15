a588d66e2c10b971cf9f4ed27d2552cb345f6a5c
shilipz <shilpaantony812@gmail.com>
2023-12-15 22:21:34 +0530

    first commit

---
+++ lib/bloc/weather_state.dart
@@ -0,0 +1,19 @@
+part of 'weather_bloc.dart';
+
+class WeatherState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+final class WeatherInitial extends WeatherState {}
+
+final class WeatherLoading extends WeatherState {}
+
+final class WeatherFailure extends WeatherState {}
+
+final class WeatherSuccess extends WeatherState {
+  final Weather weather;
+  WeatherSuccess(this.weather);
+  @override
+  List<Object> get props => [weather];
+}
---
+++ lib/data/my_data.dart
@@ -0,0 +1,1 @@
+String API_KEY = "c1e61dd3d19d8b053f577840f0d29eeb";
